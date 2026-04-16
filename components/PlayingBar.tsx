81173ec44f366826b7ba9f8dccc7fa1c1238233c
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-04-16 13:11:39 +0400

    added volume func to change vol

--- components/PlayingBar.tsx
+++ components/PlayingBar.tsx
@@ -6,7 +6,9 @@ export default function PlayingBar() {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [isRepeat, setIsRepead] = useState<boolean>(false);
   const [isActive, setIsActive] = useState<boolean>(false);
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState<number>(0);
+  const [isMute, setIsMute] = useState<boolean>(false);
+  const [volume, setVolume] = useState(50);
 
   useEffect(() => {
     if (!isPlaying) return;
@@ -185,24 +187,57 @@ export default function PlayingBar() {
         </div>
         <div className="flex-1 flex">
           <div className="flex-1 flex items-center gap-3 justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-volume2-icon lucide-volume-2"
+            <button
+              onClick={() => setIsMute((prev) => !prev)}
+              className="hover:text-white"
             >
-              <path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z" />
-              <path d="M16 9a5 5 0 0 1 0 6" />
-              <path d="M19.364 18.364a9 9 0 0 0 0-12.728" />
-            </svg>
-            <div className="w-24 flex items-center gap-3 bg-gray-600 ">
-              <div className="flex-1 h-1 bg-gray-600 rounded-full relative" />
+              {isMute ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-volume-off-icon lucide-volume-off"
+                >
+                  <path d="M16 9a5 5 0 0 1 .95 2.293" />
+                  <path d="M19.364 5.636a9 9 0 0 1 1.889 9.96" />
+                  <path d="m2 2 20 20" />
+                  <path d="m7 7-.587.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298V11" />
+                  <path d="M9.828 4.172A.686.686 0 0 1 11 4.657v.686" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-volume2-icon lucide-volume-2"
+                >
+                  <path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z" />
+                  <path d="M16 9a5 5 0 0 1 0 6" />
+                  <path d="M19.364 18.364a9 9 0 0 0 0-12.728" />
+                </svg>
+              )}
+            </button>
+            <div className="w-24 flex items-center gap-3">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                onChange={(e) => setVolume(Number(e.target.value))}
+                className="w-full"
+              />
             </div>
           </div>
         </div>
